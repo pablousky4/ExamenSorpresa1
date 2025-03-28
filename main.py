fd11d99e3642b29f0cf96673cbc6336aa85a1ebc
Pxblo <69274746+pablousky4@users.noreply.github.com>
2025-03-28 08:31:07 +0000

    hago el metodo para identificar el lugar de los cuadrantes

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ class Punto:
     
     def __str__(self):
         return f"X={self.x}, Y={self.y}"
+    
+    def cuadrante(self):
+        if self.x > 0 and self.y > 0:
+            return "Primer cuadrante"
+        elif self.x < 0 and self.y > 0:
+            "Segundo cuadrante"
+        elif self.x < 0 and self.y < 0:
+            "Tercer cuadrante"
+        elif self.x > 0 and self.y < 0:
+            "Cuarto cuadrante"
+        elif self.x == 0 and self.y != 0:
+            return "Esta en el eje Y"
+        elif self.x != 0 and self.y == 0:
+            return "Esta en el eje X"
+        else:
+            return "esta en origen"
